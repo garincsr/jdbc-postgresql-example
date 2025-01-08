8c9fd75e31b594b18ca4ea8b853ce5ea883bdb73
Garin Caesar <garincaesar69@gmail.com>
2025-01-08 13:56:40 +0700

    feat: transaction

--- src/com/enigmacamp/entitiy/dto/request/TransactionDetailRequest.java
+++ src/com/enigmacamp/entitiy/dto/request/TransactionDetailRequest.java
@@ -3,4 +3,25 @@ package com.enigmacamp.entitiy.dto.request;
 public class TransactionDetailRequest {
     private Integer productId;
     private Integer qty;
+
+    public TransactionDetailRequest(Integer qty, Integer productId) {
+        this.qty = qty;
+        this.productId = productId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
 }
--- src/com/enigmacamp/entitiy/dto/request/TransactionRequest.java
+++ src/com/enigmacamp/entitiy/dto/request/TransactionRequest.java
@@ -5,5 +5,26 @@ import java.util.List;
 public class TransactionRequest {
     private Integer customerId;
     private List<TransactionDetailRequest> trxDetails;
+
+    public TransactionRequest(Integer customerId, List<TransactionDetailRequest> trxDetails) {
+        this.customerId = customerId;
+        this.trxDetails = trxDetails;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<TransactionDetailRequest> getTrxDetails() {
+        return trxDetails;
+    }
+
+    public void setTrxDetails(List<TransactionDetailRequest> trxDetails) {
+        this.trxDetails = trxDetails;
+    }
 }
 
